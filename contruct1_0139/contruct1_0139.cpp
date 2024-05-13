c3ca23d98c2ec83b501602b41b58afe75e7f2cac
bloodfunny <adhyaakmalrafiansyah@email.com>
2024-05-13 14:14:33 +0700

    mendefinisikan member mahasiswa

--- contruct1_0139/contruct1_0139.cpp
+++ contruct1_0139/contruct1_0139.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 class Mahasiswa
 {
-public:
+private:
 	int nim;
 	string nama;
 
@@ -18,23 +18,4 @@ public:
 	Mahasiswa(string iNama) {nama = iNama;}
 	Mahasiswa(int iNim, string iNama) { nim = iNim; nama = iNama; }
 
-	void cetak() {
-		cout << endl << " Nim = " << nim << endl;
-		cout << " Nama = " << nama << endl;
-	}
-
-};
-
-int main() {
-	Mahasiswa mhs1;
-	Mahasiswa mhs2(20);
-	Mahasiswa mhs3("rafi");
-	Mahasiswa mhs4(20, "Eko");
-
-	mhs1.cetak();
-	mhs2.cetak();
-	mhs3.cetak();
-	mhs4.cetak();
-
-	return 0;
-}
+	
