cea26a8a387cffb4afd9cb5f1d3c666c28723885
bloodfunny <adhyaakmalrafiansyah@email.com>
2024-05-13 13:47:37 +0700

    mendefinisikan anggota angka

--- destruct_0139/destruct_0139.cpp
+++ destruct_0139/destruct_0139.cpp
@@ -4,10 +4,16 @@ using namespace std;
 class angka {
 private:
 	int* arr;
-	int pajang;
+	int panjang;
 public:
 	angka(int);
 	~angka();
 	void cetakData();
 	void isiData();
 };
+
+angka::angka(int i) {
+	panjang = i;
+	arr = new int[i];
+	isiData();
+}
